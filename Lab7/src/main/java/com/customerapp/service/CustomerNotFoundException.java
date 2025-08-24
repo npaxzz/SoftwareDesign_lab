f64d769131da67b6274d3421dec031d14cc484e7
paint <82027125+npaxzz@users.noreply.github.com>
2025-08-24 22:00:21 +0700

    Delete Lab7 directory

--- Lab7/src/main/java/com/customerapp/service/CustomerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.customerapp.service;
-
-public class CustomerNotFoundException extends RuntimeException {
-
-	public CustomerNotFoundException(Long id) {
-		super("Could not find customer " + id);
-	}
-}
